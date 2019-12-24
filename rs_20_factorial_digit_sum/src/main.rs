1decfa31ea46ed1570706b990661587c2db02143
kyle o'neill <kyleoneill24@gmail.com>
2019-12-24 12:19:47 -0600

    Finished problem 20

--- rs_20_factorial_digit_sum/src/main.rs
+++ rs_20_factorial_digit_sum/src/main.rs
@@ -1,26 +1,37 @@
 use std::time::{Instant};
+extern crate num_bigint;
+extern crate num_traits;
+use num_bigint::BigUint;
+use num_traits::{Zero, One};
 
 fn main() {
+    let one_hundred: BigUint = 100u32.into();
     let start = Instant::now();
-    let factorial = factorial(100);
+    let factorial = factorial(&one_hundred);
     let solution = sum_factorial_digits(factorial);
     println!("Found solution in {:?} microseconds", start.elapsed().as_micros());
     println!("The solution is: {0}", solution.to_string());
 }
 
-fn factorial(n: u128) -> u128 { // need to use a bigint here
-    match n {
-        0 => 1,
-        1 => 1,
-        _ => factorial(n - 1) * n,
+fn factorial(n: &BigUint) -> BigUint {
+    if n == &Zero::zero() {
+        One::one()
+    }
+    else if n == &One::one() {
+        One::one()
+    }
+    else {
+        let one: BigUint = One::one();
+        factorial(&(n - one)) * n
     }
 }
 
-fn sum_factorial_digits(mut f: u128 ) -> u128 {
-    let mut sum = 0;
-    while f != 0 {
-        sum += f % 10;
-        f /= 10;
+fn sum_factorial_digits(mut f: BigUint ) -> BigUint {
+    let mut sum: BigUint = Zero::zero();
+    while f != Zero::zero() {
+        let ten: BigUint = 10u32.into();
+        sum += &f % &ten;
+        f /= ten;
     }
     sum
 }
